5e86410b97014cd4b6faf456a944bdcdd0aac7e1
Widoyo <widoyo@gmail.com>
2023-08-15 13:50:21 +0700

    Nambah Google Analityc

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -5,6 +5,20 @@ export default defineConfig({
   title: "Wiratama Rental Mobil",
   description: "Dapatkan sewa mobil dengan layanan yang mudah dan efisien",
   cleanUrls: true,
+  head: [
+    [
+      'script',
+      { async: '', src: 'https://www.googletagmanager.com/gtag/js?id=G-EFMJSJ2WXX' }
+    ],
+    [
+      'script',
+      {},
+      `window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'TAG_ID');`
+    ]
+  ],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
